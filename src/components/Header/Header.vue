50efb62cf79f0fdf8f1e2701d932e385cb1da2c6
creativehimel <himel.phy@bsmrstu.edu.bd>
2023-09-30 20:47:56 +0600

    Project Done

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,7 +1,9 @@
 <script setup>
 import {RouterLink} from "vue-router";
 import {useAuthStore} from "@/store/AuthStore";
+import {ref} from "vue";
 const authStore = useAuthStore()
+const isActive = ref(false)
 </script>
 
 <template>
@@ -12,7 +14,7 @@ const authStore = useAuthStore()
           <img src="@/assets/images/logo.png" alt="Logo" class="w-12">
         </RouterLink>
 
-        <ul class="gap-x-6 font-semibold text-gray-700 text-lg hidden md:flex">
+        <ul class="gap-x-6 font-semibold text-gray-700 text-lg md:flex hidden">
           <li>
             <RouterLink to="/">Home</RouterLink>
           </li>
@@ -26,7 +28,21 @@ const authStore = useAuthStore()
             <RouterLink to="/contact">Contact</RouterLink>
           </li>
         </ul>
-        <div class="space-x-3" >
+        <ul v-if="isActive" class="font-semibold text-white text-lg flex flex-col md:hidden absolute top-[73px] space-y-3 py-6 left-0 text-center bg-indigo-600 w-full">
+          <li>
+            <RouterLink to="/">Home</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/task">Task</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/about">About</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/contact">Contact</RouterLink>
+          </li>
+        </ul>
+        <div class="space-x-3 flex items-center" >
           <button v-if="!authStore.isAuthenticated" class="text-white hover:text-indigo-700 border-2 border-indigo-400 hover:bg-white bg-indigo-500 px-8 py-2 rounded text-sm font-medium tracking-wide transition-colors duration-300">
             <RouterLink to="/login">Login</RouterLink>
           </button>
@@ -35,6 +51,12 @@ const authStore = useAuthStore()
           </button>
           <button v-if="authStore.isAuthenticated" @click="authStore.logout()" class="text-indigo-700 hover:text-white bg-white border-2 border-indigo-400 px-8 py-2 rounded text-sm font-medium tracking-wide hover:bg-indigo-600 transition-colors duration-300">
             <RouterLink to="/register">Logout</RouterLink>
+          </button>
+          <button @click="isActive = !isActive">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+            </svg>
+
           </button>
         </div>
       </nav>
