967ce353241768b03e31e0a05727231c0c704d1a
creativehimel <himel.phy@bsmrstu.edu.bd>
2023-09-30 20:20:55 +0600

    Project Done

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -17,7 +17,13 @@ const userLogin = ()=>{
 
 <template>
   <section class="bg-gray-50 dark:bg-gray-900">
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+
+    <div class="flex flex-col items-center justify-center px-6 py-3 mx-auto md:h-screen lg:py-0">
+      <div class="py-3">
+        <h3>Default Use Email and Password</h3>
+        <p>Email: admin@gmail.com</p>
+        <p>Password: 123456</p>
+      </div>
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
